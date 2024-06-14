88936b36ddc87a11e9e32268660421ed36ea11f7
EmilianoAprile <emilianoaprile77@icloud.com>
2024-06-14 14:33:33 +0200

    add > slide props

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,42 @@
-import Slide from './slide/Slide.jsx'
+import Slide from "./slide/Slide.jsx";
+import {useState} from 'react';
 
 function App() {
+  const slides = [
+    {
+      id: 1,
+      title: "Slide 1",
+      image: "/slide-1.jpg",
+      text: "Slide uno",
+    },
+    {
+      id: 2,
+      title: "Slide 2",
+      image: "/slide-2.jpg",
+      text: "Slide due",
+    },
+    {
+      id: 3,
+      title: "Slide 3",
+      image: "/slide-3.jpg",
+      text: "Slide tre",
+    }
+  ];
 
   return (
     <>
-      <div className='container'>
-        <Slide/>
+      <div className="container">
+        {slides.map((slide, index) => (
+          <Slide 
+            key={slide.id}
+            title={slide.title}
+            image={slide.image}
+            text={slide.text}
+          />
+        ))}
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
